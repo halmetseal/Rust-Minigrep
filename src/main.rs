4bbb0d86964a718304c61ff672690dea104bad97
halmetseal <omlette87@proton.me>
2025-08-10 13:34:39 +0200

    Arguments get stored in variables now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String>  = env::args().collect();
+    // dbg!(args);
+    let binary_name = &args[0];
+
+    if args.len()<3 {
+        print!("Usage: {binary_name} <searchstring> <file_path>");
+        process::exit(1)
+    }
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
+
 }
