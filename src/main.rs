cf210c01fbadceda21577eb359e70baccc9bbd10
halmetseal <omlette87@proton.me>
2025-08-11 20:55:19 +0200

    Extracting program logic into run function

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,26 @@ use std::{
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-            println!("Problem parsing arguments: {err}");
-            process::exit(1);
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
     });
+    if let Err(err) = run(config) {
+        println!("Application error: {err}");
+        process::exit(1);
+    }
+}
 
+fn run(config: Config) -> Result<(), Box<dyn std::error::Error>> {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(&config.file_path)
-        .unwrap_or_else(|_| panic!("Could not read the specified file {}", config.file_path));
+    let contents = fs::read_to_string(&config.file_path)?;
 
     println!("File contents: ");
 
     println!("{contents}");
+
+    Ok(())
 }
 
 struct Config {
@@ -29,7 +36,7 @@ struct Config {
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            return Err("not enough arguments")
+            return Err("not enough arguments");
         }
 
         let query = args[1].clone();
