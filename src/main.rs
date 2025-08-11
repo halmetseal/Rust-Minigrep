c10e8b4e4d3f58c51e96d6386d33f7c511930139
halmetseal <omlette87@proton.me>
2025-08-11 20:44:42 +0200

    Improved error handling of args parsing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::{
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -24,17 +27,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let binary_name = &args[0];
-
+    fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            print!("Usage: {binary_name} <searchstring> <file_path>");
-            process::exit(1)
+            return Err("not enough arguments")
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
