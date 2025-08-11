3f8cf288fb1d7c866ec3d96ba4e215044ca42111
halmetseal <omlette87@proton.me>
2025-08-11 20:33:39 +0200

    Extracted args parsing to Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
-use std::{env, fs, process};
+use std::{
+    env::{self},
+    fs, process,
+};
 
 fn main() {
-    let args: Vec<String>  = env::args().collect();
-    // dbg!(args);
-    let binary_name = &args[0];
+    let args: Vec<String> = env::args().collect();
+    let config: Config = Config::new(&args);
 
-    if args.len()<3 {
-        print!("Usage: {binary_name} <searchstring> <file_path>");
-        process::exit(1)
-    }
-    let query = &args[1];
-    let file_path = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path)
-        .unwrap_or_else(|_| panic!("Could not read the specified file {file_path}"));
+    let contents = fs::read_to_string(&config.file_path)
+        .unwrap_or_else(|_| panic!("Could not read the specified file {}", config.file_path));
 
     println!("File contents: ");
 
     println!("{contents}");
+}
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let binary_name = &args[0];
+
+        if args.len() < 3 {
+            print!("Usage: {binary_name} <searchstring> <file_path>");
+            process::exit(1)
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
